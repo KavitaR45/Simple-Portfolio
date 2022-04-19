77799ff2d5b213a1367dfcb11b46f383c4ae8dbe
@Kavita2599 <kavita99@netart.io>
2022-04-19 21:27:45 +0530

    error solve

--- pages/_document.js
+++ pages/_document.js
@@ -1,40 +1,30 @@
-import Document, { Head, Main, NextScript } from 'next/document';
-// Import styled components ServerStyleSheet
+import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    // Step 1: Create an instance of ServerStyleSheet
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-    // Step 2: Retrieve styles from components in the page
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />),
-    );
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-    // Step 3: Extract the styles as <style> tags
-    const styleTags = sheet.getStyleElement();
-
-    // Step 4: Pass styleTags as a prop
-    return { ...page, styleTags };
-  }
-
-  render() {
-    return (
-      <html lang='en'>
-        <head>
-          <title>Kavita Rawat - Personal Portfolio</title>
-          <meta name="description" content="I’m a FrontEnd Developer specializing in building exceptional digital experiences with technologies like ReactJs, Gatsby, NextJs, Wordpress and Woocommerce" />
-          <link rel="icon" href="/images/logo.svg" />
-          <link rel="canonical" href="https://kavita-portfolio.netlify.app/" />
-          {/* Step 5: Output the styles in the head  */}
-          {this.props.styleTags}
-        </head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -6,28 +6,35 @@ import Contact from "../components/Contact"
 import Project from "../components/Project"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
+import Head from "next/head"
 
 export default function Home() {
   return (
     <>
-      <Header/>
+      <Head>
+        <title>Kavita Rawat - Personal Portfolio</title>
+        <meta name="description" content="I’m a FrontEnd Developer specializing in building exceptional digital experiences with technologies like ReactJs, Gatsby, NextJs, Wordpress and Woocommerce" />
+        <link rel="icon" href="/images/logo.svg" />
+        <link rel="canonical" href="https://kavita-portfolio.netlify.app/" />
+      </Head>
+      <Header />
       {/* Banner Section */}
       <Banner subheading={"I build things for the web."} subtitle={"Hi, my  name  is"} title={"Kavita Rawat."} desc={"I’m a FrontEnd Developer specializing in building (and occasionally designing) exceptional digital experiences with technologies like ReactJs, Gatsby, NextJs, Wordpress and Woocommerce"} btnText="Download Resume" btnLink={"/images/Resume.pdf"} />
       <section id="about" style={{ padding: "50px 0" }}>
         <SectionHeading section={1} title={"About Me"} />
-        <About img={"/images/profile.png"} desc={<>Hello! I enjoy creating things that live on the internet. My interest in web development started back in 2019 when I decided to try creating a simple landing page - and with that my front-end journey began.<br /><br />Fast-forward to today, and I’ve had the privilege of working at a start-up. I started by fixing styles in the project, collabrated with clients and assisted senior developer, while strengthning my base with each passing projects. My main focus these days is building accessible, digital experiences for the clients without compromising the browser support and performance.<br /><br />Here are a few technologies I’ve been working with recently:<br/><br/><ul><li>JavaScript (ES6+)</li><li>ReactJs</li><li>NextJs</li><li>Gatsby</li><li>Wordpress</li><li>Woocommerce</li><li>Shopify</li><li>FireCMS & Netlify CMS</li></ul></>} />
+        <About img={"/images/profile.png"} desc={<>Hello! I enjoy creating things that live on the internet. My interest in web development started back in 2019 when I decided to try creating a simple landing page - and with that my front-end journey began.<br /><br />Fast-forward to today, and I’ve had the privilege of working at a start-up. I started by fixing styles in the project, collabrated with clients and assisted senior developer, while strengthning my base with each passing projects. My main focus these days is building accessible, digital experiences for the clients without compromising the browser support and performance.<br /><br />Here are a few technologies I’ve been working with recently:<br /><br /><ul><li>JavaScript (ES6+)</li><li>ReactJs</li><li>NextJs</li><li>Gatsby</li><li>Wordpress</li><li>Woocommerce</li><li>Shopify</li><li>FireCMS & Netlify CMS</li></ul></>} />
       </section>
       <section id="work" style={{ padding: "50px 0" }}>
         <SectionHeading section={2} title={"Some Things I’ve Built"} />
-       <Project link={"https://syncmedia.io/"} technology={"NextJs , React , ThreeJs , Parallax , ThemeUI"} img={"/images/project/sync.png"} desc={"An animation filled website based on user interaction for an advertising agency. For the animation I had used parallax and reveal effects. For the wave animation in banner, three js is used."} subtitle={"Featured Project"} title={"Sync Media"} />
-       <Project link={"https://iamfashion.co.in/"} technology={"Wordpress , Elementor , Woocommerce"} flexDirection textAlign img={"/images/project/ecom.png"} desc={"An e-Commerce clothing site for the fashion lover. It mainly focus on women clothing, the website is created in wordpress with the help of elementor and woocommerce. The site consists all the features of a professional & modern e-commerce site."} subtitle={"Featured Project"} title={"I am Fashion"} />
-       <Project link={"https://drdassans.com/"} technology={"Wordpress , Elementor , Javascript"} img={"/images/project/dassan.png"} desc={"A professional business website for an ayurvedic center. The website is created in Wordpress with the help of elementor. The site is filled with videos and images without affecting browser performance."} subtitle={"Featured Project"} title={"Dr Dassan's"} />
-         </section>
+        <Project link={"https://syncmedia.io/"} technology={"NextJs , React , ThreeJs , Parallax , ThemeUI"} img={"/images/project/sync.png"} desc={"An animation filled website based on user interaction for an advertising agency. For the animation I had used parallax and reveal effects. For the wave animation in banner, three js is used."} subtitle={"Featured Project"} title={"Sync Media"} />
+        <Project link={"https://iamfashion.co.in/"} technology={"Wordpress , Elementor , Woocommerce"} flexDirection textAlign img={"/images/project/ecom.png"} desc={"An e-Commerce clothing site for the fashion lover. It mainly focus on women clothing, the website is created in wordpress with the help of elementor and woocommerce. The site consists all the features of a professional & modern e-commerce site."} subtitle={"Featured Project"} title={"I am Fashion"} />
+        <Project link={"https://drdassans.com/"} technology={"Wordpress , Elementor , Javascript"} img={"/images/project/dassan.png"} desc={"A professional business website for an ayurvedic center. The website is created in Wordpress with the help of elementor. The site is filled with videos and images without affecting browser performance."} subtitle={"Featured Project"} title={"Dr Dassan's"} />
+      </section>
       <section id="contact" style={{ padding: "50px 0" }}>
         <SectionHeading section={3} title={"Get In Touch"} />
-        <Contact desc={<>Thank you for scrolling all this way!<br /><br />You can connect with me via email <a href="mailto:kavitarawat1214@gmail.com"><span style={{color:"#64ffda"}}>kavitarawat1214@gmail.com</span> </a>or you can drop your message in the form below.<br/><br/>My inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!</>} />
+        <Contact desc={<>Thank you for scrolling all this way!<br /><br />You can connect with me via email <a href="mailto:kavitarawat1214@gmail.com"><span style={{ color: "#64ffda" }}>kavitarawat1214@gmail.com</span> </a>or you can drop your message in the form below.<br /><br />My inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!</>} />
       </section>
-      <Footer/>
+      <Footer />
     </>
   )
 }
