1ba68ee7194fa45f33da96501378d325b9dc792a
@Kavita2599 <kavita99@netart.io>
2023-03-16 11:45:20 +0530

    Hotjar Tracking Code

--- pages/_document.js
+++ pages/_document.js
@@ -46,7 +46,7 @@ class MyDocument extends Document {
               `,
             }}
           />
-            <script
+          <script
             type="text/javascript"
             dangerouslySetInnerHTML={{
               __html: `
@@ -56,6 +56,20 @@ class MyDocument extends Document {
                     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
                 })(window, document, "clarity", "script", "g9dfynwutm");
               `,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+          (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3409788,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `,
             }}
           />
         </Head>
